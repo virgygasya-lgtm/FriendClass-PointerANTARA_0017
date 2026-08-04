537014f9216bdc548a6f700f72741e2909c757f2
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-04 10:11:23 +0700

    menambahkankelassiswabesertaatributdandeklarasifungsi.cpp

--- menambahkankelassiswabesertaatributdandeklarasifungsi.cpp
+++ menambahkankelassiswabesertaatributdandeklarasifungsi.cpp
@@ -13,3 +13,13 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
