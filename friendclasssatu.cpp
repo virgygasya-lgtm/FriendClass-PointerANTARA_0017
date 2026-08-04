2d91b53b5438f180178a3ae7148daa1b33ec5e8d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-04 10:17:08 +0700

    friendclasssatu.cpp

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -24,6 +24,12 @@ public:
     void displayAll(orang &a);
 };
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl
+         << a.nama;
+}
+
 void orang::setNama(string pNama)
 {
     nama = pNama;
@@ -34,11 +40,6 @@ void siswa::setId(int pId)
     id = pId;
 }
 
-void siswa::displayAll(orang &a)
-{
-    cout << id << endl
-         << a.nama;
-}
 int main()
 {
     orang joko;
